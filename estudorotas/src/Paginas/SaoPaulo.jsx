008976c473133186badb578366087f452f88bf9e
Mariiiliza <mpafumedasilva@gmail.com>
2025-08-05 14:52:11 -0300

    05/08/25

---
+++ estudorotas/src/Paginas/SaoPaulo.jsx
@@ -0,0 +1,29 @@
+export default function SaoPaulo ()
+
+{
+    return(
+        <div>
+            <h1>Seja bem vindo ao Estado de</h1>
+        
+
+        <p>
+            São Paulo é a maior cidade do Brasil e um importante centro financeiro e cultural. Conhecida pela diversidade cultural, abriga pessoas de diferentes partes do mundo. Além disso, é famosa pela sua gastronomia, arte urbana e vida agitada.
+            </p>
+
+            <p>
+                <img src="" />
+                </p>
+                <p>
+                    <a href="/">Voltar para Home</a>
+                    </p>
+
+</div>
+
+
+
+
+
+
+     
+    )
+}
