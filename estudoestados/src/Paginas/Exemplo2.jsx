c96646227a040f5fb7778c6c80a539d6c34ba5a8
Mariiiliza <mpafumedasilva@gmail.com>
2025-09-02 14:54:08 -0300

    02/09

--- estudoestados/src/Paginas/Exemplo2.jsx
+++ estudoestados/src/Paginas/Exemplo2.jsx
@@ -2,8 +2,31 @@ export default function Exemplo2() {
   return (
     <div>
       <h1>Exemplo 2</h1>
+      <div className="conteudo">
+        <h3>Calculo</h3>
+        <p>
+  O obejetivo será receber o nome do aluno,suas 3 notas e calcular a média.</p>
+<form>
+  <p>
+    Nome do Aluno <br />
+    <input type="text" />
+  </p>
 
+  <p>
+    Nota 1 <br />
+    <input type="text" />
+  </p>
+  <p>
+    Nota 2 <br />
+    <input type="text" />
+  </p>
+  
+  <p>
+    <input type="button" value="Calcular" />
+  </p>
+</form>
       
+      </div>
     </div>
   );
 }
